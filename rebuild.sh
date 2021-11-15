ff526cd170364c9f73a16b9a098f90a73edd886a
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-11-15 16:10:46 -0500

    minor tweaks

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #comment out git commands if you are rebuilding on your own code, sometimes git commands do strange things
 git pull
-
+sleep 2
 gcc -o edacs-fm edacs-fm.c $(ncursesw5-config --cflags --libs)
 #comment line above and uncomment below line if ncurses produces garbled ascii art, or you have newer than ncurses5, i.e. ARCH LINUX
 #gcc -o edacs-fm edacs-fm.c -lncurses
