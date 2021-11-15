16d8b77d8d0a6d2cd8fafaa59f6a8fe6a48707a6
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-11-15 13:25:52 -0500

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #comment out git pull if you are rebuilding on your own code, else leave if you want to update your own install
-git checkout .
-git pull
+#git checkout .
+#git pull
 #gcc -o ledacs-esk-analyzer ledacs-esk-analyzer.c
 #not sure if analyzer will return or not
 gcc -o edacs-fm edacs-fm.c $(ncursesw5-config --cflags --libs)
