279a9a3bd58798146c04fd03f7631c736412a15f
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-11-15 15:43:38 -0500

    Delete sample-configuration directory

--- sample-configuration/start-lcn-rtludp.sh
+++
@@ -1,1 +0,0 @@
-./rtl_udp -d 1 -f 851375000 -s 28.8k -p 0.5 -g 36 | tee >(sox -t raw -b 16 -e signed-integer -r 28800 -c 1 - -t raw - vol 2 sinc 0.2k-4.5k -a 110 rate 48000 | aplay -t raw -f S16_LE -r 48000 -c 1) | ./edacs-lcn
