53d4a4948925ad4e7aed775c477399a8f7a9d318
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-11-17 17:42:39 -0500

    remove sox, tweak for digital decoding
    remove sox, tweak for digital decoding

--- sample-configuration/start-lcn-gr.sh
+++ sample-configuration/start-lcn-gr.sh
@@ -1,2 +1,2 @@
-socat stdio udp-listen:2001 | tee >(sox -t raw -b 16 -e signed-integer -r 48000 -c 1 - -t raw - vol 2 sinc 0.2k-4.5k -a 110 rate 48000 2> /dev/null | aplay -t raw -f S16_LE -r 48000 -c 1 2> /dev/null ) | ./edacs-lcn
+socat stdio udp-listen:2001 | aplay -t raw -f S16_LE -r 48000 -c 1 2> /dev/null ) | ./edacs-lcn
 
