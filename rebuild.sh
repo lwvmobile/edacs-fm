53a991d527bd6f6fbd734a539561c23435f239a1
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-11-18 21:36:51 -0500

    blanket chmod x on any sh files

--- rebuild.sh
+++ rebuild.sh
@@ -7,4 +7,5 @@ gcc -o edacs-fm edacs-fm.c $(ncursesw5-config --cflags --libs)
 gcc -o edacs-lcn edacs-lcn.c
 
 #adding line to make these all executable again in case git pull changes it 
-chmod +x build.sh rebuild.sh start-control-gr.sh start-control-rtlfm.sh start-lcn-gr.sh start-lcn-rtludp.sh start-pyedacs-gr.sh start-sdrpp-gqrx-udp-signal.sh
+#chmod +x build.sh rebuild.sh start-control-gr.sh start-control-rtlfm.sh start-lcn-gr.sh start-lcn-rtludp.sh start-pyedacs-gr.sh start-sdrpp-gqrx-udp-signal.sh
+chmod +x *.sh
