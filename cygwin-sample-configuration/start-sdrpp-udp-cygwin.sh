10e47bd97a7d349523ee8fbefb33859b7eab6cc2
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-12-11 12:33:31 -0500

    Make startup script more interactive and friendly

--- cygwin-sample-configuration/start-sdrpp-udp-cygwin.sh
+++ cygwin-sample-configuration/start-sdrpp-udp-cygwin.sh
@@ -1,8 +1,31 @@
-
+#! /bin/bash
+clear
+echo EDACS-FM Florida Man Edition - Control Channel Interactive Startup for SDR++ UDP Signal Source.
+echo 
+echo Name of CSV file with Site Frequencies?
+echo Defaults to site.csv if no answer.
+read SITE
+Y=''
+if [[ $Y == $SITE ]]; then
+	SCSV=''
+else
+	SCSV="-s ${SITE}"
+fi	
+echo
+echo Name of CSV file with Talkgroup Information?
+echo Defaults to group.csv if no answer.
+read GROUP
+Y=''
+if [[ $Y == $GROUP ]]; then
+	GCSV=''
+else
+	GCSV="-g ${GROUP}"
+fi	
+echo
 echo Which type of System?
 echo l EDACS Standard or Networked
 echo e EDACS Standard or Networked with ESK
-echo x EDACS Extended Addressing with ESK
+echo x EDACS Extended Adressing with ESK
 echo E EDACS Extended Addressing without ESK
 read type
 TYPE1='l'
@@ -22,8 +45,8 @@ else
     A=4
     F=4
 fi
-
-#nc -l -u -p 7355 | sox -t raw -b 16 -e signed-integer -r 48000 -c 1 - -t raw - rate 28800 | ./edacs-fm -$type -s site.csv -g group.csv -a $A -f $F
-#use nc if BSD version, use socat if nc is debian or other half broken version
-socat stdio udp-listen:7355 | sox -t raw -b 16 -e signed-integer -r 48000 -c 1 - -t raw - rate 28800 | ./edacs-fm -$type -s site.csv -g group.csv -a $A -f $F -v
+echo All Display and Logging Enabled 
+echo
+read -p "Press Enter key to start"
+socat stdio udp-listen:7355 | sox -t raw -b 16 -e signed-integer -r 48000 -c 1 - -t raw - rate 28800 | ./edacs-fm -$type $SCSV $GCSV -a $A -f $F -S -C -P -Q -L
 
