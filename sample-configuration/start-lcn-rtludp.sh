dc1187813df0131d4dfeef9741cb36e00ad9b304
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-12-11 15:45:07 -0500

    Make startup script more interactive and friendly

--- sample-configuration/start-lcn-rtludp.sh
+++ sample-configuration/start-lcn-rtludp.sh
@@ -1,1 +1,30 @@
-./rtl_udp -d 1 -f 855987500 -s 28.8k -p -2.0 -g 46 2> /dev/null | tee >(sox -t raw -b 16 -e signed-integer -r 28800 -c 1 - -t raw - 2> /dev/null | aplay -t raw -f S16_LE -r 28800 -c 1 2> /dev/null ) | ./edacs-lcn
+#! /bin/bash
+clear
+echo EDACS-FM Florida Man Edition - LCN Channel using RTL_UDP.
+echo 
+./rtl_test -t
+echo Device Listing Complete. 
+echo
+echo Which Device Number would you like to use?
+read DEVICE
+echo
+echo RF Gain Value? 
+echo Typically 0-49 for RTL2838 devices.
+read GAIN
+echo
+echo PPM Value?
+read PPM
+echo
+echo Parking LCN Channel on 850M. Device is Tunable.
+STRING="./rtl_udp -d ${DEVICE} -f 850M -s 28.8k -p ${PPM} -g ${GAIN} 2> /dev/null | tee >(sox -t raw -b 16 -e signed-integer -r 28800 -c 1 - -t raw - 2> /dev/null | aplay -t raw -f S16_LE -r 28800 -c 1 2> /dev/null ) | ./edacs-lcn"
+echo
+echo Script below crafted from answers to above questions. 
+echo Copy and paste into sh file if you do not wish to answer everytime.
+echo Set sh file executable with command chmod +x example.sh
+echo
+echo "#! /bin/bash"
+echo $STRING
+echo
+read -p "Press Enter key to start"
+clear
+./rtl_udp -d $DEVICE -f 850M -s 28.8k -p $PPM -g $GAIN 2> /dev/null | tee >(sox -t raw -b 16 -e signed-integer -r 28800 -c 1 - -t raw - 2> /dev/null | aplay -t raw -f S16_LE -r 28800 -c 1 2> /dev/null ) | ./edacs-lcn
