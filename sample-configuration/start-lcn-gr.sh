ff64b23af78be0fbe77a720fb722f88101494218
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-12-11 15:46:17 -0500

    Make startup script more interactive and friendly

--- sample-configuration/start-lcn-gr.sh
+++ sample-configuration/start-lcn-gr.sh
@@ -1,2 +1,4 @@
+#! /bin/bash
+clear
 socat stdio udp-listen:2001 | tee >(sox -t raw -b 16 -e signed-integer -r 28800 -c 1 - -t raw - 2> /dev/null | aplay -t raw -f S16_LE -r 28800 -c 1 2> /dev/null ) | ./edacs-lcn
 
