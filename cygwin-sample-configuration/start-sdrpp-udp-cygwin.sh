f0e7f5a3c5c1f107583705496e87d8974b47992d
lwvmobile <59371473+lwvmobile@users.noreply.github.com>
2021-12-19 00:46:54 -0500

    Add options to interactive startup script
    Add options to interactive startup script

--- cygwin-sample-configuration/start-sdrpp-udp-cygwin.sh
+++ cygwin-sample-configuration/start-sdrpp-udp-cygwin.sh
@@ -20,6 +20,16 @@ if [[ $Y == $GROUP ]]; then
 	GCSV=''
 else
 	GCSV="-g ${GROUP}"
+fi
+Y='y'
+echo
+echo Universal Denial Mode? - Only Groups with Mode [A] in csv file allowed voice grant.
+echo y/N
+read D
+if [[ $Y == $D ]]; then
+	DENY='-d'
+else
+	DENY=''
 fi	
 echo
 echo Which type of System?
@@ -27,15 +37,23 @@ echo l EDACS Standard or Networked
 echo e EDACS Standard or Networked with ESK
 echo x EDACS Extended Addressing with ESK
 echo E EDACS Extended Addressing without ESK
+echo A EDACS Auto Detect - Experimental
 read type
 TYPE1='l'
 TYPE2='e'
+TYPE3='A'
+
 if [[ $type == $TYPE1 ]]; then
     echo Agency bit length? Default 4
     read A
     echo Fleet bit length? Default 4
     read F
 elif [[ $type == $TYPE2 ]]; then
+    echo Agency bit length? Default 4
+    read A
+    echo Fleet bit length? Default 4
+    read F
+elif [[ $type == $TYPE3 ]]; then
     echo Agency bit length? Default 4
     read A
     echo Fleet bit length? Default 4
@@ -48,5 +66,5 @@ fi
 echo All Display and Logging Enabled 
 echo
 read -p "Press Enter key to start"
-socat stdio udp-listen:7355 | sox -t raw -b 16 -e signed-integer -r 48000 -c 1 - -t raw - rate 28800 | ./edacs-fm -$type $SCSV $GCSV -a $A -f $F -S -C -P -Q -L
+socat stdio udp-listen:7355 | sox -t raw -b 16 -e signed-integer -r 48000 -c 1 - -t raw - rate 28800 | ./edacs-fm -$type $SCSV $GCSV -a $A -f $F -S -C -P -Q -L $DENY
 
